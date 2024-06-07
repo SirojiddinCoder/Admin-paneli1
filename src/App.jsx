96f6edeb8090aadd6fd3ec392a4a82efa4a46300
SirojiddinCoder <sirojiddincoder@gmail.com>
2024-06-07 00:28:43 -0700

    login page qismi tayyor deyarli

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import Category from './pages/Category'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
 
+
+  const token =  localStorage.getItem("accessToken")
+  console.log('tt',token);
 
   return (
     <>
@@ -17,6 +21,8 @@ function App() {
        <Route path="/about" element={<Category />} />
        <Route path="*" element={<Home />} />
      </Routes>
+     <ToastContainer />  
+ 
     </>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../pages/Login.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
 
 
 export const Login = () => {
+   
+
+  const navigate = useNavigate();
+
+    const [raqam,setRaqam] = useState("")
+    const [parol,setParol] = useState("")
+
+    console.log(raqam,parol);
+
+    const handleSubmit =(e)=>{
+        e.preventDefault(); 
+        fetch('https://autoapi.dezinfeksiyatashkent.uz/api/auth/signin', {
+            method: 'POST',
+            body: JSON.stringify({
+              phone_number:raqam,
+              password:parol
+            }),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+          })
+             .then((response) => response.json())
+             .then((data) => {
+                console.log(data);
+               if(data?.success===true){
+                toast.success("Muvofaqiyatli yo'naltirildi")
+                localStorage.setItem('accessToken',data?.data?.tokens?.accessToken?.token)
+
+                navigate("/home")
+
+                localStorage.setItem()
+               } else{
+                toast.error("Xatolik yuz berdi")
+                navigate("/")
+               }
+             })
+             .catch((err) => {
+                console.log(err.message);
+             });
+    }
+
+
+
+
+
   return (
     <div className='Login'>
        <div class="login-page">
        
   <div class="form">
   <h3>ADMIN- PANEL</h3>
-    <form class="register-form">
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
-      <button>create</button>
-      <p class="message">Already registered? <a href="#">Sign In</a></p>
-    </form>
     <form class="login-form">
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
-      <button>login</button>
-    
+      <input onChange={(e)=>setRaqam(e?.target?.value)} type="text" placeholder="Phone Number" required/>
+      <input onChange={(e)=>setParol(e?.target?.value)} type="text" placeholder="password" required/>
+      <button onClick={handleSubmit} type='submit'>Submit</button>
+   
     </form>
+   
   </div>
 </div>
-
+<ToastContainer />    
     </div>
+    
   )
 }
 
